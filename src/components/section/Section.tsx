f0b2f1d5012e783d749bf5c4466d0f0077c8a56e
Kite-x3 <igorevich.05@bk.ru>
2024-11-30 20:31:25 +0300

    added base markup for section

---
+++ src/components/section/Section.tsx
@@ -0,0 +1,20 @@
+export default function Section(){
+    return(
+        <section>
+            <h2>Hard skills</h2>
+            <ul>
+                <li></li>
+                <li></li>
+                <li></li>
+            </ul>
+            <h2>
+                Soft skills
+            </h2>
+            <div>
+                <li></li>
+                <li></li>
+                <li></li>
+            </div>
+        </section>
+    )
+}
