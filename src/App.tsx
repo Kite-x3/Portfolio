4b78d3748b57fbb80ec2762b19ec2b3ce092636c
SystemX <igorevich.05@bk.ru>
2024-11-30 21:34:50 +0300

    full version of section

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import './App.css'
 import Header from './components/header/Header'
 import Navigation from './components/navigation/Navigation'
+import Section from './components/section/Section'
 
 export default function App() {
   return (
     <>
       <Navigation />
       <Header />
+      <Section />
     </>
   )
 }
--- src/components/section/Section.tsx
+++ src/components/section/Section.tsx
@@ -1,20 +1,26 @@
-export default function Section(){
-    return(
-        <section>
-            <h2>Hard skills</h2>
-            <ul>
-                <li></li>
-                <li></li>
-                <li></li>
-            </ul>
-            <h2>
-                Soft skills
-            </h2>
-            <div>
-                <li></li>
-                <li></li>
-                <li></li>
-            </div>
-        </section>
-    )
+import classes from './section.module.css'
+
+export default function Section() {
+  return (
+    <section className={classes.skillsSection}>
+      <h2>Hard skills</h2>
+      <ul>
+        <li>
+          Programming languages knowledge: C++, C#, JavaScript, TypeScript;
+        </li>
+        <li>Markup languages: HTML, CSS</li>
+        <li>Understanding of library: React</li>
+        <li>Database usage skills</li>
+        <li>Knowledge of English language</li>
+      </ul>
+      <h2>Soft skills</h2>
+      <ul>
+        <li>Highly motivated to learn and grow</li>
+        <li>Ability to work effectively in a team</li>
+        <li>Purposefulness</li>
+        <li>Resilient under stress</li>
+        <li>Quick learner</li>
+      </ul>
+    </section>
+  )
 }
