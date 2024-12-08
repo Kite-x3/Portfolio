849cf1dbf4275680a4af9cfe4e680f2dcf0aa648
SystemX <igorevich.05@bk.ru>
2024-12-08 18:33:11 +0300

    fixed projects presentation and changed img of project1

--- src/components/projectList/ProjectList.tsx
+++ src/components/projectList/ProjectList.tsx
@@ -4,10 +4,13 @@ import clases from './ProjectList.module.css'
 
 export default function ProjectList() {
   return (
-    <section className={clases.projectContainer}>
-      {projects.map((proj, index) => (
-        <ProjectBlock key={index} {...proj} />
-      ))}
+    <section className={clases.projectSection}>
+      <h2 className={clases.projectSectionHead}>Projects</h2>
+      <div className={clases.projectContainer}>
+        {projects.map((proj, index) => (
+          <ProjectBlock key={index} {...proj} />
+        ))}
+      </div>
     </section>
   )
 }
