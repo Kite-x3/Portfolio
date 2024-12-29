666ca9d6faa26005bda24c0f0c2185efa118eeaa
SystemX <igorevich.05@bk.ru>
2024-12-29 20:53:44 +0300

    ?

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -6,8 +6,8 @@ export default function Footer() {
   return (
     <footer className={classes.footerInfo}>
       <div className={classes.Logos}>
-        <GithubLogo color='white' link='https://github.com/Kite-x3' />
-        <VkLogo color='white' link='https://vk.com/chervyakai' />
+        <GithubLogo link='https://github.com/Kite-x3' />
+        <VkLogo link='https://vk.com/chervyakai' />
       </div>
       <div>@2024 Frontend porfolio</div>
     </footer>
